6b92c5b2d53c34a7bc66215eb5dd3f177d19115f
Traceur0 <ghost01ghost@gmail.com>
2022-06-29 13:58:47 +0900

    fitst commit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from seleniumIns import dynamicSearch, pagination, scrapping
-from subFunc import list_up
+from seleniumIns import load_page, pagination, auto_download
 
-URL = "http://ncads.go.kr/ex/svcexec/openSvcExecAllList.do#검색" # 실제 함수가 적용되는 곳에 
 
-dynamicSearch()
-
-# last_page = pagination()
-# print(last_page)
-# list_up(last_page)
+load_page()
+last_p = pagination()
+auto_download(last_p)
